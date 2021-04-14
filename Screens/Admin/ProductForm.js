f626970922f08d977c71b05ea52ac85328227441
robert1564 <robertmaximus383@gmail.com>
2021-04-14 13:08:57 +0300

    4th commit
    Implement rest of admin page

--- Screens/Admin/ProductForm.js
+++ Screens/Admin/ProductForm.js
@@ -1,7 +1,30 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { 
+    View, 
+    Text,
+    Image,
+    StyleSheet,
+    TouchableOpacity,
+    Platform
+} from 'react-native'
+import { Item, Picker } from "native-base"
+import FormContainer from "../../Shared/Form/FormContainer"
+import Input from "../../Shared/Form/Input"
+import MyButton from "../../Shared/StyledComponents/MyButton"
+import Error from "../../Shared/Error"
+import Icon from "react-native-vector-icons/FontAwesome"
+import Toast from "react-native-toast-message"
+import AsyncStorage from "@react-native-community/async-storage"
+import baseURL from "../../assets/common/baseUrl"
+import axios from "axios"
 
 const ProductForm = (props) => {
+
+    const [pickerValue, setPickerValue] = useState();
+    const [brand, setBrand] = useState();
+    const [name, setName] = useState();
+
+
     return (
         <View>
             <Text>ProductForm Screen</Text>
