e671bd5e39f6dc9b3953e77c1c28b0c05d315102
robert1564 <robertmaximus383@gmail.com>
2021-04-15 22:51:59 +0300

    Done with update product bug and categories admin page

--- Screens/Cart/Checkout/Confirm.js
+++ Screens/Cart/Checkout/Confirm.js
@@ -11,19 +11,46 @@ import {
 import { connect } from 'react-redux'
 import * as actions from '../../../Redux/Actions/cartActions'
 
+import Toast from "react-native-toast-message"
+import axios from "axios"
+import baseURL from "../../../assets/common/baseUrl"
+
 var { width, height } = Dimensions.get('window')
 
 const Confirm = (props) =>{
 
+    const finalOrder = props.route.params;
+
     const confirmOrder = () => {
 
-        setTimeout(() => {
-            props.clearCart();
-            props.navigation.navigate("Cart")
-        }, 500)
+        const order = finalOrder.order.order;
+
+        axios
+        .post(`${baseURL}orders`, order)
+        .then((res) => {
+            if(res.status == 200 || res.status == 201){
+                Toast.show({
+                    topOffset: 60,
+                    type: "success",
+                    text1: "Order Completed",
+                    text2: "",
+                })
+                setTimeout(() => {
+                    props.clearCart();
+                    props.navigation.navigate("Cart")
+                }, 500)
+            }
+        })
+        .catch((error) => {
+            Toast.show({
+                topOffset: 60,
+                type: "error",
+                text1: "Something went wrong",
+                text2: "Please try again",
+            })
+        })
     }
 
-    const confirm = props.route.params
     return(
         <ScrollView contentContainerStyle={styles.container}>
             <View style={styles.titleContainer}>
@@ -34,14 +61,14 @@ const Confirm = (props) =>{
                 <View style={{ borderWidth: 1, borderColor: 'orange'}}>
                     <Text style={styles.title}>Shipping to:</Text>
                     <View style={{ padding: 8 }}>
-                        <Text>Address: {confirm.order.order.shippingAddress1}</Text>
-                        <Text>Address2: {confirm.order.order.shippingAddress2}</Text>
-                        <Text>City: {confirm.order.order.city}</Text>
-                        <Text>Zip Code: {confirm.order.order.zip}</Text>
-                        <Text>Country: {confirm.order.order.country}</Text>
+                        <Text>Address: {finalOrder.order.order.shippingAddress1}</Text>
+                        <Text>Address2: {finalOrder.order.order.shippingAddress2}</Text>
+                        <Text>City: {finalOrder.order.order.city}</Text>
+                        <Text>Zip Code: {finalOrder.order.order.zip}</Text>
+                        <Text>Country: {finalOrder.order.order.country}</Text>
                     </View>
                     <Text style={styles.title}>Items:</Text>
-                    {confirm.order.order.orderItems.map((x) => {
+                    {finalOrder.order.order.orderItems.map((x) => {
                         return (
                             <ListItem
                                 style={styles.listItem}
