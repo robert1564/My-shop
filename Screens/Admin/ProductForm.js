add2dfdb7d17d1ae893b7bc4def956b3bc47288c
robert1564 <robertmaximus383@gmail.com>
2021-04-15 21:48:16 +0300

    Revert "5th commit"
    This reverts commit 5b4360abd803abbba3bda03172edb19e2a37285a.

--- Screens/Admin/ProductForm.js
+++ Screens/Admin/ProductForm.js
@@ -1,210 +1,35 @@
-import React, { useState, useEffect } from "react";
-import {
-  View,
-  Text,
-  Image,
-  StyleSheet,
-  TouchableOpacity,
-  Platform,
-} from "react-native";
-import { Item, Picker } from "native-base";
-import FormContainer from "../../Shared/Form/FormContainer";
-import Input from "../../Shared/Form/Input";
-import MyButton from "../../Shared/StyledComponents/MyButton";
-import Error from "../../Shared/Error";
-import Icon from "react-native-vector-icons/FontAwesome";
-import Toast from "react-native-toast-message";
-import AsyncStorage from "@react-native-community/async-storage";
-import baseURL from "../../assets/common/baseUrl";
-import axios from "axios";
-import * as ImagePicker from "expo-image-picker";
+import React, { useState, useEffect } from 'react'
+import { 
+    View, 
+    Text,
+    Image,
+    StyleSheet,
+    TouchableOpacity,
+    Platform
+} from 'react-native'
+import { Item, Picker } from "native-base"
+import FormContainer from "../../Shared/Form/FormContainer"
+import Input from "../../Shared/Form/Input"
+import MyButton from "../../Shared/StyledComponents/MyButton"
+import Error from "../../Shared/Error"
+import Icon from "react-native-vector-icons/FontAwesome"
+import Toast from "react-native-toast-message"
+import AsyncStorage from "@react-native-community/async-storage"
+import baseURL from "../../assets/common/baseUrl"
+import axios from "axios"
 
 const ProductForm = (props) => {
-  const [pickerValue, setPickerValue] = useState();
-  const [brand, setBrand] = useState();
-  const [name, setName] = useState();
-  const [price, setPrice] = useState();
-  const [description, setDescription] = useState();
-  const [image, setImage] = useState();
-  const [mainImage, setMainImage] = useState();
-  const [category, setCategory] = useState();
-  const [categories, setCategories] = useState([]);
-  const [token, setToken] = useState();
-  const [err, setError] = useState();
-  const [countInStock, setCountInStock] = useState();
-  const [rating, setRating] = useState(0);
-  const [isFeatured, setIsFeature] = useState(false);
-  const [richDescription, setRichDescription] = useState();
-  const [numReviews, setNumReviews] = useState(0);
-  const [item, setItem] = useState(null);
 
-  useEffect(() => {
-    // Categories
-    axios
-      .get(`${baseURL}categories`)
-      .then((res) => setCategories(res.data))
-      .catch((error) => alert("Error to load categories"));
+    const [pickerValue, setPickerValue] = useState();
+    const [brand, setBrand] = useState();
+    const [name, setName] = useState();
 
-    // Image Picker
-    (async () => {
-        if (Platform.OS !== "web") {
-            const {
-                status,
-            } = await ImagePicker.requestCameraPermissionsAsync();
-            if (status !== "granted") {
-                alert("Sorry, we need camera roll permissions to make this work!")
-            }
-        }
-    })();
-    
-    return () => {
-      setCategories([]);
-    };
-  }, []);
 
-  const pickImage = async () => {
-    let result = await ImagePicker.launchImageLibraryAsync({
-        mediaTypes: ImagePicker.MediaTypeOptions.All,
-        allowsEditing: true,
-        aspect: [4, 3],
-        quality: 1
-    });
-
-    if (!result.cancelled) {
-        setMainImage(result.uri);
-        setImage(result.uri);
-    }
-};
-
-  return (
-    <FormContainer title="Add Product">
-      <View style={styles.imageContainer}>
-        <Image style={styles.image} source={{ uri: mainImage }} />
-        <TouchableOpacity onPress={pickImage} style={styles.imagePicker}>
-            <Icon style={{ color: "white"}} name="camera"/>
-        </TouchableOpacity>
-      </View>
-      <View style={styles.label}>
-        <Text style={{ textDecorationLine: "underline" }}>Brand</Text>
-      </View>
-      <Input
-        placeholder="Brand"
-        name="brand"
-        id="brand"
-        value={brand}
-        onChangeText={(text) => setBrand(text)}
-      />
-      <View style={styles.label}>
-        <Text style={{ textDecorationLine: "underline" }}>Name</Text>
-      </View>
-      <Input
-        placeholder="Name"
-        name="name"
-        id="name"
-        value={name}
-        onChangeText={(text) => setName(text)}
-      />
-      <View style={styles.label}>
-        <Text style={{ textDecorationLine: "underline" }}>Price</Text>
-      </View>
-      <Input
-        placeholder="Price"
-        name="price"
-        id="price"
-        value={price}
-        keyboardType={"numeric"}
-        onChangeText={(text) => setPrice(text)}
-      />
-      <View style={styles.label}>
-        <Text style={{ textDecorationLine: "underline" }}>Count in Stock</Text>
-      </View>
-      <Input
-        placeholder="Stock"
-        name="stock"
-        id="stock"
-        value={countInStock}
-        keyboardType={"numeric"}
-        onChangeText={(text) => setCountInStock(text)}
-      />
-      <View style={styles.label}>
-        <Text style={{ textDecorationLine: "underline" }}>Description</Text>
-      </View>
-      <Input
-        placeholder="Description"
-        name="description"
-        id="description"
-        value={description}
-        onChangeText={(text) => setDescription(text)}
-      />
-      <Item picker>
-        <Picker
-          mode="dropdown"
-          iosIcon={<Icon color={"#007aff"} name="arrow-down" />}
-          style={{ height: 50, width: 150 }}
-          placeholder="Select your Category"
-          selectedValue={pickerValue}
-          placeholderStyle={{ color: "#007aff" }}
-          placeholderIconColor="#007aff"
-          onValueChange={(e) => [setPickerValue(e), setCategory(e)]}
-        >
-          {categories.map((c) => {
-            return <Picker.Item key={c.id} label={c.name} value={c.id} />;
-          })}
-        </Picker>
-      </Item>
-      {err ? <Error message={err} /> : null}
-      <View style={styles.buttonContainer}>
-        <MyButton 
-            large 
-            primary 
-            //onPress={() => addProduct()}
-        >
-          <Text style={styles.buttonText}>Confirm</Text>
-        </MyButton>
-      </View>
-    </FormContainer>
-  );
-};
-
-const styles = StyleSheet.create({
-  label: {
-    width: "80%",
-    marginTop: 10,
-  },
-  buttonContainer: {
-    width: "80%",
-    marginBottom: 80,
-    marginTop: 20,
-    alignItems: "center",
-  },
-  buttonText: {
-    color: "white",
-  },
-  imageContainer: {
-    width: 200,
-    height: 200,
-    borderStyle: "solid",
-    borderWidth: 8,
-    padding: 0,
-    justifyContent: "center",
-    borderRadius: 100,
-    borderColor: "#E0E0E0",
-    elevation: 10,
-  },
-  image: {
-    width: "100%",
-    height: "100%",
-    borderRadius: 100,
-  },
-  imagePicker: {
-    position: "absolute",
-    right: 5,
-    bottom: 5,
-    backgroundColor: "grey",
-    padding: 8,
-    borderRadius: 100,
-    elevation: 20,
-  },
-});
+    return (
+        <View>
+            <Text>ProductForm Screen</Text>
+        </View>
+    )
+}
 
 export default ProductForm;
